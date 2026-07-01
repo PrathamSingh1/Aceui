1e562a32f3d08fa9a04197a4701fa90ccd0b07f5
PrathamSingh1 <prathamjii36@gmail.com>
2026-07-01 08:43:55 +0000

    added ace-button-demo

--- src/components/docs/ace-button-demo.tsx
+++ src/components/docs/ace-button-demo.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import React from "react";
-import AceButton from "../ui/ace-button";
+import { AceButton } from "@/components/ui/ace-button";
 
-export default function AceButtonDemo() {
+export function AceButtonDemo() {
   return (
     <div className="flex h-[300px] w-full items-center justify-center">
       <AceButton>Get Started</AceButton>
     </div>
   );
 }
+
+export default AceButtonDemo;
