e4a336eaff3215e5a281f7363ae89f495db59fce
PrathamSingh1 <prathamjii36@gmail.com>
2026-08-03 23:26:33 +0530

    added container for layouting

---
+++ src/components/layout/container.tsx
@@ -0,0 +1,11 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+export const Container = ({ children, className }: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn('max-w-7xl mx-auto', className)}>{children}</div>
+  )
+}
