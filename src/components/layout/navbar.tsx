d9aac9329436d720f6fe8e83281b02bbef3fd04a
PrathamSingh1 <prathamjii36@gmail.com>
2026-08-04 09:10:52 +0000

    improved navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -2,6 +2,7 @@
 
 import {
   IconCommand,
+  IconComponents,
   IconMoon,
   IconSearch,
   IconStar,
@@ -11,6 +12,7 @@ import React, { useEffect, useState } from "react";
 import { GithubLogo } from "../icons/icons";
 import { useTheme } from "next-themes";
 import Link from "next/link";
+import { Button } from "./button";
 
 export function Navbar() {
   const { theme, setTheme } = useTheme();
@@ -29,7 +31,9 @@ export function Navbar() {
         <div>
           {/* logo */}
           <Link href="/">
-            <h2 className="text-xl font-[700] leading-[28px] text-neutral-800 dark:text-neutral-100">Ace UI</h2>
+            <h2 className="text-xl leading-[28px] font-[700] text-neutral-800 dark:text-neutral-100">
+              Ace UI
+            </h2>
           </Link>
         </div>
         <div className="flex items-center gap-4">
@@ -44,40 +48,46 @@ export function Navbar() {
           </div>
           <div className="flex items-center gap-4">
             {/* github */}
-            <div className="flex items-center gap-2 rounded-2xl border border-neutral-300 px-2 py-1 dark:border-neutral-900 group cursor-pointer">
+            <div className="group flex cursor-pointer items-center gap-2 rounded-2xl border border-neutral-300 px-2 py-1 dark:border-neutral-900">
               <div className="flex gap-2">
                 <GithubLogo />
                 <span className="border-r border-r-neutral-800"></span>
               </div>
               <div className="flex items-center gap-1">
-                <IconStar className="h-4 w-4 dark:fill-neutral-600 fill-neutral-400 dark:stroke-neutral-600 stroke-neutral-400 group-hover:fill-amber-500 group-hover:stroke-amber-500 transition-all duration-200" />
+                <IconStar className="h-4 w-4 fill-neutral-400 stroke-neutral-400 transition-all duration-200 group-hover:fill-amber-500 group-hover:stroke-amber-500 dark:fill-neutral-600 dark:stroke-neutral-600" />
                 <p>000</p>
               </div>
             </div>
-            <div onClick={() => setTheme(theme === "dark" ? "light" : "dark")} className="relative">
-              <button
-                className="size-4 flex items-center justify-center relative rounded-md bg-neutral-200 dark:bg-neutral-800 p-3.5 cursor-pointer active:scale-[0.97] shadow-[inset_0_0_0_2px_hsl(0_0%_85%)] dark:shadow-[inset_0_0_0_2px_hsl(0_0%_20%)]"
+            <div
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+              className="relative"
             >
+              <button className="relative flex size-4 cursor-pointer items-center justify-center rounded-md bg-neutral-200 p-3.5 shadow-[inset_0_0_0_2px_hsl(0_0%_85%)] active:scale-[0.97] dark:bg-neutral-800 dark:shadow-[inset_0_0_0_2px_hsl(0_0%_20%)]">
                 {theme === "dark" ? (
-                  <IconMoon size = { 12 }  className="rotate-90 absolute scale-0 transition-all duration-200 dark:rotate-0 dark:scale-100" />
-
-              ) : (
-                    <IconSun size={12} className="rotate-0 absolute scale-100 transition-all duration-200 dark:rotate-90 dark:scale-0" />
-
-              )}
+                  <IconMoon
+                    size={12}
+                    className="absolute scale-0 rotate-90 transition-all duration-200 dark:scale-100 dark:rotate-0"
+                  />
+                ) : (
+                  <IconSun
+                    size={12}
+                    className="absolute scale-100 rotate-0 transition-all duration-200 dark:scale-0 dark:rotate-90"
+                  />
+                )}
               </button>
             </div>
-            <div className="px-3 py-2 dark:hover:bg-neutral-800 hover:bg-neutral-200 rounded-md cursor-pointer transition-colors duration-200 -mr-2">
+            <div className="-mr-2 cursor-pointer rounded-md px-3 py-2 transition-colors duration-200 hover:bg-neutral-200 dark:hover:bg-neutral-800">
               <h2>Docs</h2>
             </div>
             <div className="h-4 border-r border-neutral-600"></div>
-            <div className="px-3 py-2 dark:hover:bg-neutral-800 hover:bg-neutral-200 rounded-md cursor-pointer transition-colors duration-200 -ml-2">
+            <div className="-ml-2 cursor-pointer rounded-md px-3 py-2 transition-colors duration-200 hover:bg-neutral-200 dark:hover:bg-neutral-800">
               <h2>Templates</h2>
             </div>
-            <Link href="/components">
-              <button className="cursor-pointer rounded-lg border border-neutral-300 bg-neutral-200 px-2 py-2 font-semibold text-neutral-800 text-shadow-lg hover:bg-neutral-300 active:scale-[0.97]">
-                All Component
-              </button>
+            <Link href={`/components`}>
+              <Button className="flex items-center gap-2">
+                All Components
+                <IconComponents size={16} />
+              </Button>
             </Link>
           </div>
         </div>
